ad41c4d8517cdf2f4f51a339f608bc313d8bcf7c
Roman Podlinov <rpodlinov@gmail.com>
2021-06-03 12:05:07 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@
 gsettings get org.gnome.desktop.lockdown disable-lock-screen
 
 # install base packages
-sudo apt install -y mc vim git apt-transport-https curl
+sudo apt install -y mc vim git apt-transport-https curl \
+         dirmngr gnupg ca-certificates software-properties-common
 
 # install brave browser
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
@@ -13,6 +14,15 @@ sudo apt update
 sudo apt install -y brave-browser
 
 # install remmina
-sudo apt-add-repository ppa:remmina-ppa-team/remmina-next
+sudo apt-add-repository -y ppa:remmina-ppa-team/remmina-next
 sudo apt update
 sudo apt install -y remmina remmina-plugin-rdp remmina-plugin-secret
+
+# install Sublime 3
+curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo add-apt-repository -y "deb https://download.sublimetext.com/ apt/stable/"
+sudo apt install -y sublime-text
+
+# install KePassXC
+sudo add-apt-repository -y ppa:phoerious/keepassxc
+sudo apt update && sudo apt install -y keepassxc
